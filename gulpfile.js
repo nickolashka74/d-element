413455d9b1f3f005b9c0d0349b25368d6a973a64
nickolashka <xy4u74@yandex.ru>
2021-08-06 00:27:19 +0500

    modal complete

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ let path = {
 	src: {
 		favicon: src_folder + "/img/favicon.{jpg,png,svg,gif,ico,webp}",
 		html: [src_folder + "/*.html", "!" + src_folder + "/_*.html"],
-		js: [src_folder + "/js/app.js", src_folder + "/js/vendors.js"],
+		js: src_folder + "/js/app.js",
 		css: src_folder + "/scss/style.scss",
 		images: [src_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}", "!**/favicon.*"],
 		fonts: src_folder + "/fonts/*.ttf"
